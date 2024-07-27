1166b61bc23658e94d4c1b9706917a29e48c8259
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-07-27 23:15:15 +0300

    edited view added another meme

--- memes/models.py
+++ memes/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class Memes(models.Model):
     def __str__(self) -> str:
         return self.name
-    image = models.ImageField(upload_to='memes/files/images/',blank=True, null=True)
+    type_choises = (('image', 'image'),('video', 'video'), ('gif', 'gif'), ('all', 'all'))
     name = models.CharField(max_length=225)
+    type = models.CharField(max_length=225, choices=type_choises, blank=True, null=True)
+    image = models.ImageField(upload_to='memes/files/images/',blank=True, null=True)
+    video = models.FileField(upload_to='memes/files/images/',null=True, blank=True)
+    video_link = models.CharField(max_length=225,null=True, blank=True)
     season = models.PositiveSmallIntegerField(null=True, blank=True)
     episode = models.PositiveSmallIntegerField(null=True, blank=True)
     tags = models.CharField(max_length=225,null=True, blank=True)
-    video = models.CharField(max_length=225,null=True, blank=True)
-    # video = models.FileField(upload_to='memes/files/images/',null=True, blank=True)
+
 
     #to know which user added this 
     # user = models.ForeignKey()
